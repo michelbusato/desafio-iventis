cff04f15121d5e52cc194a703fc6e6b504998c55
Michel Busato <michel.busato@fazenda.br.gov.br>
2024-09-02 16:11:04 -0300

    Update ApiApplicationTests.java

--- src/test/java/br/com/desafio/api/ApiApplicationTests.java
+++ src/test/java/br/com/desafio/api/ApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.gozde.dockercomposedemo;
+package br.com.desafio.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DockerComposeDemoApplicationTests {
+class ApiApplicationTests {
 
     @Test
     void contextLoads() {
